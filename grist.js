99bcc901bf1b217cf30b37852e0fa7c6e88121d7
Mathilde-c31 <mathilde.cambournac@laas.fr>
2026-06-29 11:25:18 +0200

    Remove grist records handling code

--- grist.js
+++ grist.js
@@ -1,20 +1,2 @@
 // grist.js
-
-window.gristRecords = [];
-
-grist.ready({
-  requiredAccess: "read table"
-});
-
-grist.onRecords((records) => {
-
-  console.log("=== Données Grist reçues ===");
-  console.table(records);
-
-  window.gristRecords = records;
-
-  document.dispatchEvent(
-    new CustomEvent("recordsLoaded")
-  );
-
-});
+console.log("grist.js chargé");
